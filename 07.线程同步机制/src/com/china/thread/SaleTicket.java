7d4dcca49e8076dc7f660cd6385f396e4877fb30
china wu <18896820691@163.com>
2020-07-30 13:52:20 +0800

    线程同步机制-同步方法

--- 07.线程同步机制/src/com/china/thread/SaleTicket.java
+++ 07.线程同步机制/src/com/china/thread/SaleTicket.java
@@ -2,19 +2,19 @@ package com.china.thread;
 
 /**
  * @Author: china wu
- * @Description:
+ * @Description: 线程同步机制--同步方法
  * @Date: 2020/7/30 12:59
  */
 public class SaleTicket {
     public static void main(String[] args) {
-        TicketThread ticketThread = new TicketThread();
-        new Thread(ticketThread, "1号窗口").start();
-        new Thread(ticketThread, "2号窗口").start();
-        new Thread(ticketThread, "3号窗口").start();
+        TicketRunnable runnable = new TicketRunnable();
+        new Thread(runnable, "1号窗口").start();
+        new Thread(runnable, "2号窗口").start();
+        new Thread(runnable, "3号窗口").start();
     }
 }
 
-class Ticket implements Runnable {
+class TicketRunnable implements Runnable {
 
     private int ticketNum = 10;
 
@@ -23,12 +23,21 @@ class Ticket implements Runnable {
         sale();
     }
 
-    private void sale() {
+    /**
+     * 同步方法：加上synchronized修饰符，默认给this加锁
+     */
+    private synchronized void sale() {
         boolean flag = true;
-        if (ticketNum <= 0) {
-            flag = false;
-        }
         while (flag) {
+            if (ticketNum <= 0) {
+                flag = false;
+                return;
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(Thread.currentThread().getName() + "卖出" + ticketNum-- + "号票");
         }
     }
